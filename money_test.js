8c42631cb3cba053ad736950a6cd65de50090af8
CodeMachine0121 <CodeMachine0121@gmail.com>
2023-03-04 20:00:39 +0800

    feat: add division and multiplication feature done

--- money_test.js
+++ money_test.js
@@ -1,18 +1,42 @@
 const assert = require('assert');
 
-class Dollar {
-
-    constructor(number) {
+class Money {
+    constructor(number, currency) {
        this.amount = number;
+       this.currency = currency;
     }
 
     times(multiplier) {
-       return new Dollar(this.amount*multiplier);
+       return new Money(this.amount*multiplier, this.currency);
+    }
+
+    divide(divider) {
+        return new Money(this.amount/divider, this.currency);
     }
 }
 
-var fiver = new Dollar(5);
-var tenner = fiver.times(2);
-assert.strictEqual(tenner.amount, 10);
 
+class testMoneyModule {
 
+    dollar_should_be_able_to_do_times_calculate_with_currency() {
+        var eurFiver = new Money(5, "EUR");
+        let eurTenner = eurFiver.times(2);
+        var expectedEurTenner = new Money(10,"EUR");
+        assert.deepStrictEqual(eurTenner, expectedEurTenner);
+    }
+
+    dollar_should_be_able_to_do_divide_calculate_with_currency() {
+        var krwTenner = new Money(10, "KRW");
+        var krwFiver = krwTenner.divide(2);
+        var expectedKrwFiver = new Money(5, "KRW");
+        assert.deepStrictEqual(krwFiver, expectedKrwFiver);
+    }
+
+    RunAllTest() {
+        this.dollar_should_be_able_to_do_times_calculate_with_currency()
+        this.dollar_should_be_able_to_do_divide_calculate_with_currency()
+    }
+}
+
+var moneyModule = new testMoneyModule();
+moneyModule.RunAllTest();
